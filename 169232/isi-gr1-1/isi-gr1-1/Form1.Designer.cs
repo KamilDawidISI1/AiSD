9ab85663080fe1650421993cb7e27ed62047807a
CytroxCoder <115219628+KamilDawidISI1@users.noreply.github.com>
2023-10-04 11:16:41 +0200

    Add files via upload

---
+++ 169232/isi-gr1-1/isi-gr1-1/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace isi_gr1_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.nudLiczbaN = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLiczbaN)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(205, 221);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(79, 25);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // nudLiczbaN
+            // 
+            this.nudLiczbaN.Location = new System.Drawing.Point(185, 161);
+            this.nudLiczbaN.Name = "nudLiczbaN";
+            this.nudLiczbaN.Size = new System.Drawing.Size(120, 23);
+            this.nudLiczbaN.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 258);
+            this.Controls.Add(this.nudLiczbaN);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudLiczbaN)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnStart;
+        private NumericUpDown nudLiczbaN;
+    }
+}
---
+++ 169232/isi-gr1-1/isi-gr1-1/Form1.cs
@@ -0,0 +1,35 @@
+namespace isi_gr1_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int liczbaN = (int)nudLiczbaN.Value;
+            
+            int wynik = Fib(liczbaN);
+            MessageBox.Show(wynik.ToString());
+        }
+        
+        int Fib(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            
+            if (n == 1)
+            {
+                return 1;
+            }
+             
+            return Fib(n-1) + Fib(n-2); 
+            
+        } 
+
+    }
+}
